f383d2e75e28323638b41d1fadac0d60bbc9ac73
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-06-07 00:22:59 -0300

    feat: post component (comment form)

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -30,14 +30,24 @@ export function Post() {
         </p>
 
         <p>
-          👉 <a href="">jane.design/doctorcare</a>
+          👉 <a href="#">jane.design/doctorcare</a>
         </p>
 
         <p>
-          <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
-          <a href="">#rocketseat</a>
+          <a href="#">#novoprojeto</a> <a href="#">#nlw</a>{" "}
+          <a href="#">#rocketseat</a>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder="Deixe um comentário" />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   );
 }
