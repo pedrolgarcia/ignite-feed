efdd01dbde389ef1a395e59037c657b44fb504e7
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-06-06 23:33:41 -0300

    feat: sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+
 import "./global.css";
+
+import styles from "./App.module.css";
 
 export function App() {
   return (
     <div>
       <Header />
-      <h1>Hello World!</h1>
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <p>Post1</p>
+          <p>Post2</p>
+        </main>
+      </div>
     </div>
   );
 }
