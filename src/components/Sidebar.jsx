09a837943b33621031b2e3cfa266d8aa6150fc63
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-06-09 23:07:29 -0300

    feat: avatar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,7 @@
 import { PencilLine } from "phosphor-react";
 
+import { Avatar } from "./Avatar";
+
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
@@ -11,10 +13,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/pedrolgarcia.png"
-        />
+        <Avatar src="https://github.com/pedrolgarcia.png" />
 
         <strong>Pedro Lopes</strong>
         <span>Web Developer</span>
