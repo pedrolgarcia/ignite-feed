7519c91f2cf4b52ac328f475d01d0a3db993ecdd
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-06-14 23:52:24 -0300

    feat: dinamic posts

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,45 @@ import "./global.css";
 
 import styles from "./App.module.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/pedrolgarcia.png",
+      name: "Pedro Lopes",
+      role: "FullStack @ SixCreative"
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀"
+      },
+      { type: "link", content: "jane.design/doctorcare" }
+    ],
+    publishedAt: new Date("2022-06-03 20:00:00")
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/maykbrito.png",
+      name: "Mayk Brito",
+      role: "Eduacador @ Rocketseat"
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀"
+      },
+      { type: "link", content: "jane.design/doctorcare" }
+    ],
+    publishedAt: new Date("2022-06-10 20:00:00")
+  }
+];
+
 export function App() {
   return (
     <div>
@@ -14,8 +53,13 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map((post) => (
+            <Post
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          ))}
         </main>
       </div>
     </div>
