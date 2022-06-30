5fe3e653a12e13d90c07e3d0ac105f519cec7185
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-06-29 22:33:32 -0300

    chore: typescripting and fixing errors

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -5,7 +5,12 @@ import { Avatar } from "./Avatar";
 
 import styles from "./Comment.module.css";
 
-export function Comment({ content, onDeleteComment }) {
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
   const [likeCount, setLikeCount] = useState(0);
 
   function handleDeleteComment() {
@@ -18,7 +23,7 @@ export function Comment({ content, onDeleteComment }) {
 
   return (
     <div className={styles.comment}>
-      <Avatar hasBorder={false} src="https://github.com/maykbrito.png" />
+      <Avatar hasBorder={false} src="https://github.com/maykbrito.png" alt="" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from "react";
 import { format, formatDistanceToNow } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
 
@@ -7,7 +7,24 @@ import { Comment } from "./Comment";
 
 import styles from "./Post.module.css";
 
-export function Post({ author, content, publishedAt }) {
+interface Author {
+  name: string;
+  role: string;
+  avatarUrl: string;
+}
+
+interface Content {
+  type: string;
+  content: string;
+}
+
+interface PostProps {
+  author: Author;
+  content: Content[];
+  publishedAt: Date;
+}
+
+export function Post({ author, content, publishedAt }: PostProps) {
   const [comments, setComments] = useState(["Post muito bacana, hein?!"]);
 
   const [newCommentText, setNewCommentText] = useState("");
@@ -25,19 +42,23 @@ export function Post({ author, content, publishedAt }) {
     addSuffix: true
   });
 
-  function handleCreateNewComment() {
+  function handleCreateNewComment(event: FormEvent) {
     event.preventDefault();
 
     setComments([...comments, newCommentText]);
     setNewCommentText("");
   }
 
-  function handleNewCommentChange() {
+  function handleNewCommentChange(event: ChangeEvent<HTMLTextAreaElement>) {
     event.target.setCustomValidity("");
     setNewCommentText(event.target.value);
   }
 
-  function deleteComment(commentToDelete) {
+  function handleNewCommentInvalid(event: InvalidEvent<HTMLTextAreaElement>) {
+    event.target.setCustomValidity("Esse campo é obrigatório.");
+  }
+
+  function deleteComment(commentToDelete: string) {
     // imutabilidade -> as variáveis não sofrem mutação, nós criamos um novo valor (um novo espaço na memória).
 
     var commentsWithoutDeletedOne = comments.filter(
@@ -45,10 +66,6 @@ export function Post({ author, content, publishedAt }) {
     );
 
     setComments(commentsWithoutDeletedOne);
-  }
-
-  function handleNewCommentInvalid() {
-    event.target.setCustomValidity("Esse campo é obrigatório.");
   }
 
   var isNewCommentEmpty = newCommentText.length === 0;
